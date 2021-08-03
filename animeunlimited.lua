9ee656ec65ca0f4f2d8a56e705cc3ef7b3381657
Kiraaa <77504888+Kiraaa02@users.noreply.github.com>
2021-08-03 18:38:36 +0800

    Update animeunlimited.lua

--- animeunlimited.lua
+++ animeunlimited.lua
@@ -1,5 +1,5 @@
 -- No security games is so fun
-while Wait() do
+while Wait(3) do
 if getgenv().treasure == false then break end
 for i,v in pairs(game:GetService("Workspace").Treasure:GetDescendants()) do
 if v:IsA("ClickDetector") then
